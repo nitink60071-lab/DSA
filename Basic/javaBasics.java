8e13567b8d0879148307be63f031abf2b93b7d31
Nitin Kumar <nitink60071@gmail.com>
2026-09-10 11:58:12 +0530

    Update

--- Basic/javaBasics.java
+++ Basic/javaBasics.java
@@ -86,5 +86,7 @@ public class javaBasics {
         float newTotal = total + (0.18f * total);
         System.out.println("Bill with 18% gst :" + newTotal);
 
+        System.out.println("Harsh");
+
     }
 }
