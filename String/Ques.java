80ebc69f04ea5ec1019cd04bade45cc42b9cc31d
Nitin Kumar <nitink60071@gmail.com>
2026-09-11 23:49:16 +0530

    Changes

--- String/Ques.java
+++ String/Ques.java
@@ -8,9 +8,58 @@ public class Ques {
         }
     }
 
+    // Count of Vowels =>
+    static int getVowelsCount( String str1) {
+        int count = 0;
+        for(int i=0; i<str1.length(); i++) {
+            char ch = str1.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+                count ++;
+            }
+        }
+        return count;
+    }
 
-    static void main() {
-       String str = "Nitin";
-       printString(str);
+    //Reverse of String =>
+    static String reverseString(String str2) {
+        String reverse = "";
+        int n = str2.length();
+        for(int i=n-1; i>=0; i--) {
+            char ch = str2.charAt(i);
+            reverse = reverse + ch;
+        }
+        return reverse;
+    }
+
+    //check palindrome or not =>
+    static boolean checkPalindrome(String str3) {
+        String reverse ="";
+        int n = str3.length();
+        for(int i=n-1; i>=0; i--) {
+            char ch = str3.charAt(i);
+            reverse = reverse + ch;
+        }
+        if(str3.equals(reverse)) {
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+    
+
+
+    public static void main(String[] args) {
+       /* String str = "Nitin";
+       printString(str);  */
+
+       /* String str1 = "Hello India, Welcome to Delhi";
+       System.out.println(getVowelsCount(str1));  */
+
+       /* String str2 = "Hello";
+       System.out.println(reverseString(str2)); */
+
+       String str3 = "nitin";
+       System.out.println(checkPalindrome(str3));
     }
 }
