dd9ef33da355700c93599723edce408b00a95e3a
Nitin Kumar <nitink60071@gmail.com>
2026-09-12 19:27:17 +0530

    update

---
+++ BasicMaths/BasicMaths.java
@@ -0,0 +1,54 @@
+public class BasicMaths {
+    //Print Digits =>
+    static void printDigits(int num) {
+        while(num != 0) {
+            int digit = num % 10;
+            System.out.println(digit);
+            num = num/10;
+        }
+    }
+
+    static int countDigits(int num) {
+        int count = 0;
+        while(num != 0) {
+            int digit = num % 10;
+            count ++;
+            num = num/10;
+        }
+        return count;
+    }
+
+    static int sumDigits(int num) {
+        int sum = 0;
+        while( num != 0) {
+            int digit = num%10;
+            sum = sum + digit;
+            num = num/10;
+        }
+        return sum;
+    }
+
+    static int reverseNum(int num) {
+        int reverse = 0;
+        while( num != 0) {
+            int digit = num %10;
+            reverse = (reverse*10) + digit;
+            num = num/10;
+        }
+        return reverse;
+
+    }
+    static void main() {
+        /*int num = 67543;
+        printDigits(num); */
+
+        /* int num = 786549;
+        System.out.println(countDigits(num));  */
+
+        /*int num = 55464;
+        System.out.println(sumDigits(num));*/
+
+        int num = 7689;
+        System.out.println("Reverse of number: " + reverseNum(num));
+    }
+}
